83cdeb7f9a6cdf08898a7f0e76bdb0ac82c5ded0
A10Donweb <nebulae4u@gmail.com>
2026-06-12 17:02:58 +0530

    Update _store.js

--- api/_store.js
+++ api/_store.js
@@ -1,18 +1,34 @@
+const https = require('https');
+
 const UPSTASH_URL = process.env.https://huge-oyster-114646.upstash.io;
 const UPSTASH_TOKEN = process.env.gQAAAAAAAb_WAAIgcDI2MTVkOWVjNjc5NDY0MTA4YTBmMDI1ZTg5NmU2NjUxMA;
 
-async function redis(command, ...args) {
-  const url = `${UPSTASH_URL}/${[command, ...args].map(encodeURIComponent).join('/')}`;
-  const res = await fetch(url, {
-    method: 'GET',
-    headers: { 
-      Authorization: `Bearer ${UPSTASH_TOKEN}`,
-      'Content-Type': 'application/json'
-    },
+function redis(command, ...args) {
+  return new Promise((resolve, reject) => {
+    const path = '/' + [command, ...args].map(encodeURIComponent).join('/');
+    const url = new URL(UPSTASH_URL);
+    const options = {
+      hostname: url.hostname,
+      path: path,
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${UPSTASH_TOKEN}`,
+      },
+    };
+    const req = https.request(options, (res) => {
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => {
+        try {
+          const json = JSON.parse(data);
+          if (json.error) reject(new Error(json.error));
+          else resolve(json.result);
+        } catch (e) { reject(e); }
+      });
+    });
+    req.on('error', reject);
+    req.end();
   });
-  const data = await res.json();
-  if (data.error) throw new Error(data.error);
-  return data.result;
 }
 
 module.exports = { redis };
